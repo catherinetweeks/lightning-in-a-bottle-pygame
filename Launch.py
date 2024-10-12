aa6aae553136b73e767ba3344719c2cea9e0a6bc
Catherine Weeks <catherinetweeks@gmail.com>
2024-10-11 23:41:57 -0400

    background now loads as the background image

--- Launch.py
+++ Launch.py
@@ -2,9 +2,14 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((960, 540))
 clock = pygame.time.Clock()
 running = True
+
+#Load images and sounds
+backgroundpng = pygame.image.load("images/background.png")
+background = pygame.transform.scale(backgroundpng, (960, 540))
+
 
 while running:
     # poll for events
@@ -14,7 +19,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.blit(background, (0,0))
 
     # RENDER YOUR GAME HERE
 
